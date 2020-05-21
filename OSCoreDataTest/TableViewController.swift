e9a5617e1af7de4c5fd9bb0d21eea9b102e68fd1
Serhii <777sirj777@gmail.com>
2020-05-21 22:17:17 +0300

    create task

--- OSCoreDataTest/TableViewController.swift
+++ OSCoreDataTest/TableViewController.swift
@@ -1,15 +1,16 @@
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     
-    var tasks = [String]()
+    var tasks = [Task]()
 
     @IBAction func saveTask(_ sender: UIBarButtonItem) {
         let alertController = UIAlertController(title: "New task", message: "Please add new task", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { action in
             let tf = alertController.textFields?.first
-            if let newTask = tf?.text {
-                self.tasks.insert(newTask, at: 0)
+            if let newTaskTitle = tf?.text {
+                self.saveTask(withTitle: newTaskTitle)
                 self.tableView.reloadData()
             }
         }
@@ -20,6 +21,25 @@ class TableViewController: UITableViewController {
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
         present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: -Func
+    
+    private func saveTask(withTitle title: String) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -48,7 +68,8 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        cell.textLabel?.text = tasks[indexPath.row]
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
         
         return cell
     }
