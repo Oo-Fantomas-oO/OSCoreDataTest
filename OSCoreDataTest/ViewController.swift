59fba2b47c36bb1297351c7b4407a71517ad4f09
Fantomas <1234556789>
2020-05-21 11:56:42 +0300

    Initial Commit

---
+++ OSCoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OSCoreDataTest
+//
+//  Created by Onopriienko.Sergii on 21.05.2020.
+//  Copyright © 2020 Onopriienko.Sergii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
